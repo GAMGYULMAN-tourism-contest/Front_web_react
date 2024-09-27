cc6cd0981302ae6814c1ce799ce6a03b8ed62f3d
ChanMinLee2 <dlcksals87@gmail.com>
2024-09-28 02:18:34 +0900

    🎨 FIX (2024_kakao) : netlify hosting setting

--- 2024_kakao_travel/model_1/vite.config.js
+++ 2024_kakao_travel/model_1/vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // define: {
+//   //   global: {},
+//   // },
+// });
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   global: {},
-  // },
+  build: {
+    outDir: 'dist', // 기본 빌드 디렉토리
+  },
 });
